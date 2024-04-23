ad0c03a0bacfa45e82a459f48bd244e55a5cda35
elton-datasc <elton-guilherme@hotmail.com>
2024-04-22 23:26:05 -0300

    ruff check

--- tests/test_df_creator.py
+++ tests/test_df_creator.py
@@ -1,26 +1,35 @@
-import pytest
-import pandas as pd
 import pandera as pa
-from df_creator.df_creator import creator # Ensure to import your df_creator function
+
+from df_creator.df_creator import (
+    creator,
+)
 
 # Define the expected schema using Pandera
-schema = pa.DataFrameSchema({
-    "Nome": pa.Column(str),  # Validate that all values in 'Nome' are strings
-    "Idade": pa.Column(int)  # Validate that all values in 'Idade' are integers
-})
+schema = pa.DataFrameSchema(
+    {
+        'Nome': pa.Column(
+            str
+        ),  # Validate that all values in 'Nome' are strings
+        'Idade': pa.Column(
+            int
+        ),  # Validate that all values in 'Idade' are integers
+    }
+)
+
 
 def test_df_creator():
     # Call the function to create the DataFrame
     df = creator()
-    
+
     # Validate the DataFrame against the schema
     # This will raise an error if the validation fails
     schema.validate(df)
-    
+
     # If no exceptions are raised, the test passes
     assert True  # This line is optional, as the test will fail on exception
 
-# Optionally, you can add more specific tests, such as checking for non-empty DataFrame
+
+# Optionally, you can add more specific tests,such as checking for non-empty Df
 def test_df_non_empty():
     df = creator()
     assert not df.empty
