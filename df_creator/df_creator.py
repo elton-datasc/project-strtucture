f0260a5af299cab5cfaa78f092af4915bbcf625b
elton-datasc <elton-guilherme@hotmail.com>
2024-04-22 23:22:27 -0300

    correção ruff

--- df_creator/df_creator.py
+++ df_creator/df_creator.py
@@ -7,10 +7,9 @@ age = [42, 25, 32]
 
 def creator(*args):
 
-        # Criando um DataFrame a partir das listas usando zip
-        df = pd.DataFrame(list(zip(names, age)), columns=['Nome', 'Idade'])
+    df = pd.DataFrame(list(zip(names, age)), columns=['Nome', 'Idade'])
 
-        # Exibindo o DataFrame
-        return df
+    return df
+
 
 print(creator())
