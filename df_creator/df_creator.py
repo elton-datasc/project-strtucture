71a9a0de4ffb2988d127654ba9fa68847e85608e
elton-datasc <elton-guilherme@hotmail.com>
2024-04-24 18:53:24 -0300

    add docstrings e type hints

--- df_creator/df_creator.py
+++ df_creator/df_creator.py
@@ -1,14 +1,19 @@
+"""Criação dos dataframes."""
 import pandas as pd
 
 names = ['fulano', 'ciclano', 'beltrano']
-
 age = [42, 25, 32]
 
 
-def creator(*args):
+def creator() -> pd.DataFrame:
+    """
+    Cria um DataFrame a partir de listas predefinidas contendo nomes e idades.
 
+    Returns:
+        pd.DataFrame: Um DataFrame contendo duas colunas, 'Nome' e 'Idade',
+                      com os dados das listas `names` e `age`.
+    """
     df = pd.DataFrame(list(zip(names, age)), columns=['Nome', 'Idade'])
-
     return df
 
 
